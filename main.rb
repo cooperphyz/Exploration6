cc9fab9610e1307ba65b39bdc89bf116ac497a0b
cooperphyz <gjcooper99@gmail.com>
2022-05-04 19:15:10 -0700

    Rename foreground_color to remove naming duplication

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
 def draw_button(label_text, x, y, foreground_color, is_dark_mode)
-    paint(label_text, x, y, foreground_color - foreground_color(), color_code(is_dark_mode))
+    paint(label_text, x, y, foreground_color - foreground_color_modifier(), color_code(is_dark_mode))
 end
 
 def color_code(is_dark_mode=nil)
@@ -9,7 +9,7 @@ def color_code(is_dark_mode=nil)
       '#E0E0E0'
 end
 
-def foreground_color(is_dark_mode=nil)
+def foreground_color_modifier(is_dark_mode=nil)
     if is_dark_mode == true
       10
     else 
